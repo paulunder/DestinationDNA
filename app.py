7ec3f0a981998a679fc5399f8da86a71e7f19004
paul under <gameon.padawan@gmail.com>
2024-11-06 18:11:32 +0000

    upadted the app.py file - typos fixed

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-import streamlit as st
 from app_pages.multipage import MultiPage
 
-# load pages scripts
+# load pages
 from app_pages.page_summary import page_summary_body
-from app_pages.page_churned_customer_study import page_destination_customer_study_body
-from app_pages.page_prospect import page_prospect_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_predict_churn import page_predict_prospect_body
+from app_pages.page_eda import page_eda_body
+from app_pages.page_hypotheses import page_hypotheses_body
+from app_pages.page_predictor import page_predictor_body
+from app_pages.page_decision_tree_model import page_decision_tree_model_body
+from app_pages.page_conclusions import page_conclusions_body
 
-app = MultiPage(app_name= "Destination DNA")
+# Initialize the app
+app = MultiPage(app_name="Mountain vs Beach Preferences Classifier")
 
-app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Exploratory Data Analysis", page_destination_customer_study_body)
-app.add_page("Prospect Destination Preference", page_prospect_body)
-app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Prospect Preference", page_predict_prospect_body)
+# Add pages to the app
+app.add_page("Project Summary", page_summary_body)
+app.add_page("Exploratory Data Analysis", page_eda_body)
+app.add_page("Project Hypotheses", page_hypotheses_body)
+app.add_page("Preference Predictor", page_predictor_body)
+app.add_page("Decision Tree Model", page_decision_tree_model_body)
+app.add_page("Project Conclusions", page_conclusions_body)
 
-app.run() # Run the  app
+# Run the app
+app.run()
